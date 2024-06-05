a3c7b4f5e3b204d05dfb4b0802412d7f9aaa793c
Oleg A. Khlybov <fougas@mail.ru>
2024-06-05 23:07:35 +0500

    Fixes & zip archive support

--- fileworks.rb
+++ fileworks.rb
@@ -1,3 +1,6 @@
+# Gems: down seven-zip archive-zip
+
+
 require 'digest'
 require 'ostruct'
 require 'pathname'
@@ -13,13 +16,13 @@ class Remote < OpenStruct
   PROGRESS = ->(_) { print '.' }
 
   # -> Local
-  def fetch(destination = nil, file: nil, progress: PROGRESS, force: false)
+  def fetch(destination = '.', file: nil, progress: PROGRESS, force: false)
     require 'down'
-    file = destination.nil? ? local : File.join(destination, local) if file.nil?
+    file = File.join(destination, local)
     if force || !verify(file)
       # Local file either non-existent or fails the checksum test - refetch needed
       Down.download(remote, destination: file, progress_proc: progress)
-      raise "checksum verification failure" if !verify(file)
+      raise 'checksum verification failure' if !verify(file)
     end
     Local.new(file)
   end
@@ -61,18 +64,23 @@ class Local < Pathname
   EXTRACTORS = {
     /.7z$/ => ->(source, destination) {
       require 'seven_zip_ruby'
-      SevenZipRuby::Reader.extract_all(source, destination)
-    }
+      File.open(source, 'rb') { |stream| SevenZipRuby::Reader.extract_all(stream, destination) }
+    },
+    /.zip$/ => ->(source, destination) {
+      require 'archive/zip'
+      Archive::Zip.extract(source, destination)
+    },
   }
 
-  def extract(destination = nil)
+  #
+  def extract(destination = '.')
     EXTRACTORS.each do |rx, code|
       if rx.match(to_s)
         code.(to_s, destination)
-        return true
+        return
       end
     end
-    false
+    raise 'unsupported archive type'
   end
 
 end
@@ -88,4 +96,20 @@ Fileworks::Remote.new(
   arch: 'x86',
   url: 'https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-#{version}-#{release}/rubyinstaller-#{version}-#{release}-#{arch}.7z',
   sha256: '406597dca85bf5a9962f887f3b41a7bf7bf0e875c80efc8cee48cab9e988f5fa'
+).fetch#.extract
+
+
+Fileworks::Remote.new(
+  version: '1.66.0',
+  arch: 'windows-386',
+  url: 'https://downloads.rclone.org/v#{version}/rclone-v#{version}-#{arch}.zip',
+  sha256: 'ca647f69c6bf2e831902a8bd9c5f4d16f7014314d5eeb94bd3a5389a92806de8'
+).fetch#.extract
+
+
+Fileworks::Remote.new(
+  version: '0.16.4',
+  arch: 'windows_386',
+  url: 'https://github.com/restic/restic/releases/download/v#{version}/restic_#{version}_#{arch}.zip',
+  sha256: '46d932ff5e5ca781fb01d313a56cf4087f27250fbdc0d7cb56fa958476bb8af8'
 ).fetch.extract
